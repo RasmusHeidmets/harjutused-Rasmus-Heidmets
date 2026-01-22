2a30706410917ed6c8d4d32a4837090d3dcc12e6
Opilane TTHK <opilane@tthk.ee>
2026-01-22 09:39:18 +0200

    fail
    tegin koodi

---
+++ failid/file operation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace file_operation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kirjuta siia oma postkasti aadress");
+            string filepath = "C:\\Users\\opilane\\source\\repos\\harjutused Rasmus Heidmets\\failid\\file operation\\postkastaaadress.txt";
+            string aadress = Console.ReadLine();
+
+            File.WriteAllText(filepath, aadress);
+        }
+    }
+}
---
+++ tsüklite töö Program.cs
@@ -0,0 +1,44 @@
+﻿
+//Console.WriteLine("mis on teie nimi?");
+//int nimi = 0;
+//nimi = int.Parse(Console.ReadLine());
+//do
+//{
+//    Console.WriteLine("mis on teie nimi?");
+//    nimi++;
+
+//}
+//while (nimi > 0);
+//{
+//    Console.WriteLine("mis on teie nimi");
+//}
+
+
+
+
+int parool = 0;
+Console.WriteLine("sisestage parool");
+parool = int.Parse(Console.ReadLine());
+{ 
+while (parool < 1000 && parool > 9999)
+
+    Console.WriteLine("parool ei sobi");
+}
+
+if (parool > 1000 && parool < 9999)
+{ 
+    Console.WriteLine("parool sobib");
+}
+
+
+
+
+
+
+//int puuviljad = 6;
+
+//for (int k = 1; k < puuviljad; k++) 
+
+
+
+//    Console.WriteLine(k);      
